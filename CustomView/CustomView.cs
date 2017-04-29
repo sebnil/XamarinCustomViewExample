81fe94484e712d14e77273557aedb2e570fc0685
Sebastian Nilsson <sebnil@gmail.com>
2017-04-29 11:43:11 +0200

    initial commit.

---
+++ CustomView/CustomView.cs
@@ -0,0 +1,50 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using System.ComponentModel;
+
+namespace CustomViewExample
+{
+	[DesignTimeVisible(true)]  // This makes it visible in the Custom Controls panel in the iOS Designer.  The code-behind file already has the Register attribute 
+	public partial class CustomView : UIView, IComponent  // IComponent is necessary to check DesignMode
+	{
+		public CustomView(IntPtr handle) : base(handle)
+		{
+		}
+
+		#region IComponent implementation
+
+		public ISite Site { get; set; }
+		public event EventHandler Disposed;
+
+		#endregion
+
+		// This is an example of exposing a custom property that can be set via the iOS Designer
+		[Export("Name"), Browsable(true)]
+		public string Name { get; set; }
+
+		public override void AwakeFromNib()
+		{
+			base.AwakeFromNib();
+
+			if ((Site != null) && Site.DesignMode)
+			{
+				// Bundle resources aren't available in DesignMode
+				return;
+			}
+
+			NSBundle.MainBundle.LoadNib("CustomView", this, null);
+
+
+			// use bounds not frame or it'll be offset
+			this.Frame = Bounds;
+			this.rootView.Frame = Bounds;
+
+			// At this point all of the code-behind properties should be set, specifically rootView which must be added as a subview of this view
+			this.AddSubview(this.rootView);
+
+			// here's how you can use custom properties to set values on the code-behind properties
+			this.nameLabel.Text = Name;  
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Project Title
+
+Example project showing how to create a custom control (UIView) with XIB so that it's available in Xamarin iOS designer.
+Custom UIView does not render in storyboard, but can be selected from the toolbox window. If you make rendering in storyboard work, please make a pull request and I will merge it into this repo. 
+The reason to this example project simply is that I was looking for an example like this but could not find something that was working out of the box. 
+
+## Getting Started
+
+Clone and run .sln in Visual Studio.
+
+### Prerequisites
+
+Visual Studio (Tested on CE 2017)
+
+## Author
+
+* **Sebastian Nilsson** - *This project* - [sebastiannilsson.com](http://sebastiannilsson.com)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* **Various forum comments in this thread** - *Initial work* - [forums.xamarin.com](https://forums.xamarin.com/discussion/48628/how-to-create-a-custom-control-uiview-with-xib-so-that-its-available-in-xamarin-ios-designer)
+* https://developer.xamarin.com/recipes/ios/general/templates/using_the_ios_view_xib_template/
+* https://developer.xamarin.com/guides/ios/user_interface/designer/ios_designable_controls_walkthrough/
+* https://app.pluralsight.com/
